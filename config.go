33393f1d85d7c8d4981707a1d3882e576c7da3d4
ganesh-kachare-josh <ganesh.kachare@joshsoftware.com>
2025-02-21 01:31:39 +0530

    add db config file

---
+++ config.go
@@ -0,0 +1,57 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	
+	"github.com/joho/godotenv"
+)
+
+type databaseConfig struct {
+	host     string
+	port     string
+	user     string
+	password string
+	dbname   string
+}
+
+func initDB(ctx context.Context) (*sql.DB, error) {
+
+	// This loads the .env file
+	err := godotenv.Load() 
+	if err != nil {
+		return nil, errors.New("error in loading .env file")
+	}
+
+	dbConfig := databaseConfig{
+		host:     os.Getenv("db_host"),
+		port:     os.Getenv("db_port"),
+		user:     os.Getenv("db_user"),
+		password: os.Getenv("db_password"),
+		dbname:   os.Getenv("dbname"),
+	}
+
+	databaseString := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		dbConfig.host, dbConfig.port, dbConfig.user, dbConfig.password, dbConfig.dbname,
+	)
+	// Opening the db connection.
+	db, err := sql.Open("postgres", databaseString)
+
+	if err != nil {
+		return nil, fmt.Errorf("error in opening database connection: %v", err)
+	}
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("error pinging database: %v", err)
+	}
+
+	log.Println("Database connection established successfully")
+	return db, nil
+
+}
