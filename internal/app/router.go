2ecde34eca6003d199d31ad4931d0f673321a163
ganesh-kachare-josh <ganesh.kachare@joshsoftware.com>
2025-02-22 12:16:26 +0530

    add route of get request by id

---
+++ internal/app/router.go
@@ -0,0 +1,17 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/ganesh-kachare-josh/GameON/internal/app/request"
+	"github.com/gorilla/mux"
+)
+
+func NewRouter(deps Dependencies) *mux.Router {
+	router := mux.NewRouter()
+	
+	// Routes.
+	router.HandleFunc("/request/{id}" , request.GetRequestById(deps.RequestService)).Methods(http.MethodGet)
+	return router
+
+} 
