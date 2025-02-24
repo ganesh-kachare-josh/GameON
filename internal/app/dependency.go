650d8a75a8f68ba651db417ef437035e0ac3a958
ganesh-kachare-josh <ganesh.kachare@joshsoftware.com>
2025-02-24 16:41:53 +0530

    add dependencies of authentication

--- internal/app/dependency.go
+++ internal/app/dependency.go
@@ -3,20 +3,27 @@ package app
 import (
 	"database/sql"
 
+	"github.com/ganesh-kachare-josh/GameON/internal/app/auth"
 	"github.com/ganesh-kachare-josh/GameON/internal/app/request"
 	"github.com/ganesh-kachare-josh/GameON/internal/repository"
 )
 
 type Dependencies struct {
 	RequestService request.Service
+	AuthService auth.Service
 }
 
 func NewServices(db *sql.DB) (Dependencies) {
 	requestRepo := repository.NewRepo(db) 
 	requestService := request.NewService(requestRepo) 
 
+	authRepo := repository.NewAuthRepo(db)
+	authService := auth.NewService(authRepo) 
+
+
 	return Dependencies{
 		RequestService: requestService,
+		AuthService: authService,
 	}
 }
 
