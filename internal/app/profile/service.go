29901bc2eaef6eeb7477b17e688863ab68e02a1b
ganesh-kachare-josh <ganesh.kachare@joshsoftware.com>
2025-02-27 16:11:41 +0530

    add service code to get userById

---
+++ internal/app/profile/service.go
@@ -0,0 +1,29 @@
+package profile
+
+import (
+	"context"
+
+	"github.com/ganesh-kachare-josh/GameON/internal/repository"
+)
+
+type service struct {
+	profileRepo repository.ProfileRepo
+}
+
+type Service interface {
+	GetUserById(ctx context.Context, user_id int) (UserData, error)
+}
+
+func (s *service) GetUserById(ctx context.Context, user_id int) (UserData, error) {
+	response, err := s.profileRepo.GetUserById(ctx, user_id)
+	if err != nil {
+		return UserData{}, err
+	}
+	return UserData(response), nil
+}
+
+func NewService(profileRepo repository.ProfileRepo) Service {
+	return &service{
+		profileRepo: profileRepo,
+	}
+}
